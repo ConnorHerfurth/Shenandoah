443ce5b9e4c3741698720651f409480c8b08e5f5
Netherman555 <connorherfurth555@gmail.com>
2024-06-16 19:27:55 -0400

    Add project files.

---
+++ ShenandoahTest/ShenandoahTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ShenandoahTest
+{
+	TEST_CLASS(ShenandoahTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
